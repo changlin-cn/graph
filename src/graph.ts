1983b0da4735c416206fb1c5e4728b8f5121362c
Hechanglin <542177932@qq.com>
2021-01-11 00:09:32 +0800

    获取路径

--- src/graph.ts
+++ src/graph.ts
@@ -1,48 +1,103 @@
-interface vertice {
+interface vertex {
   name: string;
+  [index: string]: unknown;
 }
 interface edgeInfo {
   distance: number;
 }
 interface edge extends edgeInfo {
-  vertice: vertice;
+  vertex: vertex;
 }
 /**
  * 图
  */
-export class graph {
+export class Graph {
   /**
    *
    * @param {object[]} vertices -顶点
    * @param {string} vertice.name -顶点名
    */
-  constructor(vertices: vertice[]) {
+  constructor(vertices: vertex[]) {
     this.vertices = vertices || [];
   }
-  private vertices: vertice[];
-  private edges: { [index: string]: edge[] } = {};
+  /**
+   * 顶点
+   */
+  vertices: vertex[] = [];
+
+  /**
+   * 边
+   */
+  edges: { [index: string]: edge[] } = {};
   /**
    * 添加边
-   * @param {string} verticeName1 -顶点1
-   * @param {string} verticeName2 -顶点2
+   * @param {string} vertexName1 -顶点1
+   * @param {string} vertexName2 -顶点2
    * @param {Object} edgeInfo -边信息
    * @param {number} edgeInfo.distance -边距离
    */
-  addEdge = (verticeName1: string, verticeName2: string, edgeInfo: edgeInfo): void => {
-    const v1 = this.vertices.find((v) => v.name === verticeName1);
-    const v2 = this.vertices.find((v) => v.name === verticeName2);
+  addEdge = (vertexName1: string, vertexName2: string, edgeInfo: edgeInfo): void => {
+    const v1 = this.vertices.find((v) => v.name === vertexName1);
+    const v2 = this.vertices.find((v) => v.name === vertexName2);
     if (!v1 || !v2) {
-      throw new Error(`Can't find ${verticeName1} or ${verticeName2}`);
+      throw new Error(`Can't find ${vertexName1} or ${vertexName2}`);
     }
     this._addEdge(v1, v2, edgeInfo);
     this._addEdge(v2, v1, edgeInfo);
   };
-  private _addEdge = (vertice1: vertice, vertice2: vertice, edgeInfo: edgeInfo): void => {
-    if (!this.edges[vertice1.name]) {
-      this.edges[vertice1.name] = [];
+  /**
+   * 获取点到点的位置
+   * @param {string} vertexFrom -起始点
+   * @param {string} vertexTo -结束点
+   * @returns {Array}
+   */
+  getPath = (vertexFrom: string, vertexTo: string): { path: string; distance: number }[] => {
+    const vf = this.vertices.find((v) => v.name === vertexFrom);
+    const vt = this.vertices.find((v) => v.name === vertexTo);
+
+    if (!vf || !vt) {
+      throw new Error(`Can't find ${vertexFrom} or ${vertexTo}`);
     }
-    if (!this.edges[vertice1.name].some((n) => n.vertice.name === vertice2.name)) {
-      this.edges[vertice1.name].push({ vertice: vertice2, ...edgeInfo });
+    const temp = [];
+    const paths = [...this.edges[vf.name].map((n) => [n])];
+
+    while (paths.length) {
+      const currentPath = paths.shift() as edge[];
+      const currentVertex = currentPath[currentPath.length - 1];
+
+      if (currentVertex.vertex.name === vertexTo) {
+        temp.push(currentPath);
+        continue;
+      }
+
+      const edges = this.edges[currentVertex.vertex.name];
+
+      for (let j = 0; j < edges.length; j++) {
+        if (!currentPath.some((n) => n.vertex.name === edges[j].vertex.name) && edges[j].vertex.name !== vertexFrom) {
+          paths.push(currentPath.concat(edges[j]));
+        }
+      }
+    }
+    const result = temp.map((n) => {
+      let distance = 0;
+      let path: string = vertexFrom;
+
+      n.forEach((v) => {
+        path += `-${v.vertex.name}`;
+        distance += v.distance;
+      });
+
+      return { path, distance };
+    });
+    return result;
+  };
+
+  private _addEdge = (vertex1: vertex, vertex2: vertex, edgeInfo: edgeInfo): void => {
+    if (!this.edges[vertex1.name]) {
+      this.edges[vertex1.name] = [];
+    }
+    if (!this.edges[vertex1.name].some((n) => n.vertex.name === vertex2.name)) {
+      this.edges[vertex1.name].push({ vertex: vertex2, ...edgeInfo });
     }
   };
 }
